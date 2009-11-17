85ea2b076b4562395b5e276253f4d066d4aa600c
cacciatc <chris.cacciatore@gmail.cm>
2009-11-17 10:25:28 -0800

    Finished simple memory ops tests.

--- tests/algorithms/simple_memory_ops.rb
+++ tests/algorithms/simple_memory_ops.rb
@@ -75,6 +75,33 @@ class SimpleMemoryOpsTest < Test::Unit::TestCase
 		assert_equal("00",fstring[$m_line].split(" ")[4])
 	end
 	
+	def test_move_32_bits_of_memory_iteratively
+		i = [0xA9,0x02,0x85,0x1,0x85,0x02,0x85,0x03,0x85,0x04]
+		instr = [0xA2,0x03,0xB5,0x01,0x95,0x05,0xCA,0x10,0xFB]
+		write_input_file(i + instr)
+		system(".././2a03 #{@input_file} #{@temp_file}")
+		f = File.new(@temp_file)
+		fstring = f.readlines
+		f.close
+		assert_equal("80",fstring[$status_line].chomp)
+		assert_equal("02",fstring[$m_line].split(" ")[5])
+		assert_equal("02",fstring[$m_line].split(" ")[6])
+		assert_equal("02",fstring[$m_line].split(" ")[7])
+		assert_equal("02",fstring[$m_line].split(" ")[8])
+	end
+	
+	def test_set_16_bit_constant
+		instr = [0xA9,0x08,0x85,0x00,0xA9,0x08,0x85,0x01]
+		write_input_file(instr)
+		system(".././2a03 #{@input_file} #{@temp_file}")
+		f = File.new(@temp_file)
+		fstring = f.readlines
+		f.close
+		assert_equal("00",fstring[$status_line].chomp)
+		assert_equal("08",fstring[$m_line].split(" ")[1])
+		assert_equal("08",fstring[$m_line].split(" ")[2])
+	end	
+
 	def teardown
 		system("rm #{@temp_file} #{@input_file}")
 	end
